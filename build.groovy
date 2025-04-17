29da6ec6e22ecfc56458d082ccfaea1e43ed086a
Antoine Taillefer <ataillefer@nuxeo.com>
2025-04-17 23:39:40 +0200

    fix

--- build.groovy
+++ build.groovy
@@ -120,7 +120,7 @@ def send(Map args = [:]) {
     if (committers) {
       def entities = []
       def atCommitters = []
-      committers.each { committer -> 
+      committers.each { committer ->
         def atCommitter = "<at>${committer}</at>"
         entities.add([
           'type': 'mention',
