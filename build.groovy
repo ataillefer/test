7597019a72df1974c5ec5dfa40daf63e20be4587
Antoine Taillefer <ataillefer@nuxeo.com>
2025-05-06 17:19:35 +0200

    test job name 1

--- build.groovy
+++ build.groovy
@@ -1,217 +1,26 @@
 library identifier: "platform-ci-shared-library@v0.0.53"
 
-JIRA_URL = nxJira.getServerBrowseURL()
-
-TEAMS_CHANNEL_IDS = [
-  'Connectors': '19:e3ffdf6fcdfc45a4b5727be1672d328c@thread.tacv2',
-  'Platform': '19:817f8655db3745389cb64b4f4db5cc18@thread.tacv2',
-]
-
 def getJobView() {
-  def parentItems = currentBuild.fullProjectName.split('/').collect {
-    projectName -> Jenkins.instance.getItem(projectName)
+  def parents = currentBuild.fullProjectName.split('/').collect {
+    name -> Jenkins.instance.getItem(name)
   }.findAll {
     item -> item != null
   }
   def views = Jenkins.instance.views.findAll {
       view -> view.displayName != 'All'
   }
-  for (item in parentItems) {
+  for (item in parents) {
+    echo "item = ${item}"
     for (view in views) {
       if (view.contains(item)) {
           def viewName = view.displayName
-          // nxUtils.log(message: "View: ${viewName} contains job: ${item.displayName}")
+          echo "View: ${viewName} contains job: ${item.displayName}"
           return viewName
-          break
       }
     }
   }
   return null
 }
-
-def getChannelId() {
-  def view = getJobView()
-  return TEAMS_CHANNEL_IDS[view] ?: TEAMS_CHANNEL_IDS['Platform']
-}
-
-def getChange(commitMessage) {
-  def parts = commitMessage.split(':', 2)
-  if (parts.size() > 1) {
-    def jiraIssue = parts[0]
-    def message = parts[1]
-    return "[${jiraIssue}](${JIRA_URL}${jiraIssue}): ${message}"
-  } else {
-    return commitMessage
-  }
-}
-
-def getUserEmail(fullName) {
-  def parts = fullName.split(' ', 2)
-  if (parts.size() <=1) {
-    // nxUtils.log(message: "Cannot get email for user full name: ${fullName}")
-    return null
-  }
-  return "${parts[0]}.${parts[1]}@hyland.com"
-}
-
-def send(Map args = [:]) {
-    def channelId = args.channelId ?: getChannelId()
-    def title = args.title // required
-    def description = args.description
-    def icon = args.icon
-    def iconColor = args.iconColor ?: 'default'
-    def message = args.message
-    def changes = args.changes
-    def committers = args.committers
-    // to not display any actions, pass an empty list
-    def actions = args.actions == null ? [
-      [name: 'View build', url: "${BUILD_URL}"],
-    ] : actions
-    
-    // Microsoft Adaptive card JSON defintion with its minimum content: post title
-    def adaptiveCard = [
-      'channelId': channelId,
-      'content': [
-        'type': 'AdaptiveCard',
-        '\$schema': 'http://adaptivecards.io/schemas/adaptive-card.json',
-        'msTeams': [
-          'width': 'full'
-        ],
-        'body': [
-          [
-            'type': 'ColumnSet',
-            'columns': [
-              [
-                'type': 'Column',
-                'spacing': 'small',
-                'items': [
-                  [
-                    'type': 'TextBlock',
-                    'text': title,
-                    'wrap': true,
-                    'size': 'large',
-                    'weight': 'bolder'
-                  ]
-                ]
-              ]
-            ]
-          ]
-        ],
-        'actions': []
-      ]
-    ]
-    
-    def content = adaptiveCard['content']
-    def body = content['body']
-    def headerColumns = body[0]['columns']
-
-    if (description) {
-      headerColumns[0]['items'].add([
-        'type': 'TextBlock',
-        'text': description,
-        'wrap': true,
-        'isSubtle': true,
-        'spacing': 'none'
-      ]
-      )
-    }
-
-    if (icon) {
-      headerColumns.add(0, [
-        'type': 'Column',
-        'width': 'auto',
-        'items': [[
-          'type': 'Icon',
-          'name': icon,
-          'size': 'small',
-          'style': 'filled',
-          'color': iconColor
-        ]]
-      ])
-    }
-
-    if (message) {
-      body.add([
-        'type': 'TextBlock',
-        'text': message,
-        'wrap': true,
-        'spacing': 'small'
-      ])
-    }
-
-    if (changes) {
-      def text = changes.collect {
-        change -> "\n- ${change}"
-      }.join('')
-      body.add([
-        'type': 'TextBlock',
-        'text': "**Changes:** ${text}",
-        'wrap': true,
-        'spacing': 'small'
-      ])
-    }
-
-    if (committers) {
-      def entities = []
-      def atCommitters = []
-      committers.unique().collect { committer -> [
-        'name': committer,
-        'at': "<at>${committer}</at>",
-        'email': getUserEmail(committer)
-      ]}.findAll { committer ->
-        committer['email'] != null
-      }.each { committer ->
-        entities.add([
-          'type': 'mention',
-          'text': committer['at'],
-          'mentioned': [
-            'id': committer['email'],
-            'name': committer['name']
-          ]
-        ])
-        atCommitters.add(committer['at'])
-      }
-      content['msTeams']['entities'] = entities
-      def text = atCommitters.join(', ')
-      body.add([
-        'type': 'TextBlock',
-        'text': "**Committers:** ${text}",
-        'wrap': true,
-        'spacing': 'small'
-      ])
-    }
-
-    actions.each {
-      action -> content['actions'].add([
-        'type': 'Action.OpenUrl',
-        'title': action.name,
-        'url': action.url
-      ])
-    }
-    
-    def payload = writeJSON(
-        json: adaptiveCard,
-        returnText: true,
-        pretty: 4
-    )
-
-    // nxUtils.log(message: "JSON payload for Teams webhook: ${payload}")
-    echo "JSON payload for Teams webhook: ${payload}"
-
-    withCredentials([string(credentialsId: 'teams', variable: 'TEAMS_WEBHOOK_URL')]) {
-      httpRequest(
-        url: TEAMS_WEBHOOK_URL,
-        httpMode: 'POST',
-        acceptType: 'APPLICATION_JSON',
-        contentType: 'APPLICATION_JSON',
-        timeout: 10, // seconds
-        requestBody: payload
-      )
-    }
-}
-
-def changes = []
-def committers = []
 
 pipeline {
   agent any
@@ -220,54 +29,9 @@ pipeline {
       steps {
         echo 'Hello World'
         script {
-          echo "channelId = ${getChannelId()}"
-          currentBuild.description = 'Build 2025.1.14'
-          for (changeLogSet in currentBuild.changeSets) {
-            def repositoryBrowser = changeLogSet.browser
-            for (item in changeLogSet.items){
-              def link = repositoryBrowser.getChangeSetLink(item).toString()
-              if (repositoryBrowser.getChangeSetLink(item).toString().startsWith(GIT_URL)) {
-                changes.add(getChange(item.comment.trim()))
-                committers.add(item.authorName)
-              }
-            }
-          }
+          getJobView()
         }
       }
-    }
-  }
-  post {
-    success {
-      send(
-        title: "Build success: nuxeo/nuxeo-lts #${BUILD_NUMBER}",
-        description: currentBuild.description,
-        icon: 'CheckmarkCircle',
-        iconColor: 'good',
-        message: "Successfully built nuxeo-lts on branch ${GIT_BRANCH}",
-        changes: changes,
-        committers: committers
-      )
-      send(
-        title: "Release LTS 2021.69",
-        icon: 'CheckmarkCircle',
-        iconColor: 'good',
-        message: "LTS 2021.69 and 2021-HF69 are released and online."
-      )
-      send(
-        title: "Build failure: nuxeo/nuxeo-lts #${BUILD_NUMBER}",
-        description: currentBuild.description,
-        icon: 'ErrorCircle',
-        iconColor: 'attention',
-        message: "Failed to build nuxeo-lts on branch ${GIT_BRANCH}",
-        changes: changes,
-        committers: committers
-      )
-      send(
-        title: "Release LTS 2021.60",
-        icon: 'ErrorCircle',
-        iconColor: 'attention',
-        message: "Failed to release Nuxeo 2021.60 from build 2021.60.7"
-      )
     }
   }
 }
