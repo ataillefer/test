f860480590f1ae4e6964bbab08648fc86c54637f
Antoine Taillefer <ataillefer@nuxeo.com>
2025-05-06 18:49:51 +0200

    fix views

--- build.groovy
+++ build.groovy
@@ -3,35 +3,51 @@ library identifier: "platform-ci-shared-library@v0.0.53"
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
 
 def getJobView() {
-  def topLevelProject = currentBuild.fullProjectName.split('/', 2)[0]
-  echo "topLevelProject = ${topLevelProject}"
-  if (topLevelProject) {
-    def topLevelItem = Jenkins.instance.getItem(topLevelProject)
-    if (topLevelItem) {
-      echo "topLevelItem = ${topLevelItem}"
-      def views = Jenkins.instance.views.findAll {
-        view -> view.displayName != 'All'
-      }.each { view ->
-        if (view.contains(topLevelItem)) {
-          def viewName = view.displayName
-          echo "View: ${viewName} contains job: ${topLevelItem.displayName}"
-          return viewName
-        }
+  def views = Jenkins.instance.views.findAll {
+    view -> view.displayName != 'All'
+  }
+  def currentJob = Jenkins.instance.getItemByFullName(currentBuild.fullProjectName)
+  while (!(currentJob instanceof Hudson)) {
+    echo "currentJob = ${currentJob}"
+    echo "currentJob.displayName = ${currentJob.displayName}"
+    for (view in views) {
+      if (view.contains(currentJob)) {
+        def viewName = view.displayName
+        echo "View: ${viewName} contains job: ${currentJob.displayName}"
+        return viewName
       }
     }
+    currentJob = currentJob.parent
+    echo "currentJob.parent=${currentJob}"
   }
+  // def topLevelProject = currentBuild.fullProjectName.split('/', 2)[0]
+  // echo "topLevelProject = ${topLevelProject}"
+  // if (topLevelProject) {
+  //   def topLevelItem = Jenkins.instance.getItem(topLevelProject)
+  //   if (topLevelItem) {
+  //     echo "topLevelItem = ${topLevelItem}"
+  //     def views = Jenkins.instance.views.findAll {
+  //       view -> view.displayName != 'All'
+  //     }.each { view ->
+  //       if (view.contains(topLevelItem)) {
+  //         def viewName = view.displayName
+  //         echo "View: ${viewName} contains job: ${topLevelItem.displayName}"
+  //         return viewName
+  //       }
+  //     }
+  //   }
+  // }
   return null
 }
 
 def getJobName() {
   def currentJob = Jenkins.instance.getItemByFullName(currentBuild.fullProjectName)
-  echo "currentJob = ${currentJob}"
-  if (currentJob instanceof WorkflowMultiBranchProject) {
-    echo "is multibranch"
-    echo "currentJob.parent.displayName=${currentJob.parent.displayName}"
+  if (currentJob.parent instanceof WorkflowMultiBranchProject) {
+    echo "multibranch"
+    echo "parentJobName=${currentJob.parent.displayName}"
   } else {
     echo "regular"
-    echo "currentJob.displayName=${currentJob.displayName}"
+    echo "jobName=${currentJob.displayName}"
   }
 }
 
