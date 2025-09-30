1bef149edc714c3c2ad3757fa791dde863fe1aa2
Antoine Taillefer <ataillefer@nuxeo.com>
2025-09-30 16:37:33 +0200

    test disableConcurrentBuilds(abortPrevious: true)

--- build.groovy
+++ build.groovy
@@ -1,66 +1,15 @@
 library identifier: "platform-ci-shared-library@v0.0.53"
 
-import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
-
-def getJobView() {
-  def views = Jenkins.instance.views.findAll {
-    view -> view.displayName != 'All'
-  }
-  def currentJob = Jenkins.instance.getItemByFullName(currentBuild.fullProjectName)
-  while (!(currentJob instanceof Hudson)) {
-    echo "currentJob = ${currentJob}"
-    echo "currentJob.displayName = ${currentJob.displayName}"
-    for (view in views) {
-      if (view.contains(currentJob)) {
-        def viewName = view.displayName
-        echo "View: ${viewName} contains job: ${currentJob.displayName}"
-        return viewName
-      }
-    }
-    currentJob = currentJob.parent
-    echo "currentJob.parent=${currentJob}"
-  }
-  // def topLevelProject = currentBuild.fullProjectName.split('/', 2)[0]
-  // echo "topLevelProject = ${topLevelProject}"
-  // if (topLevelProject) {
-  //   def topLevelItem = Jenkins.instance.getItem(topLevelProject)
-  //   if (topLevelItem) {
-  //     echo "topLevelItem = ${topLevelItem}"
-  //     def views = Jenkins.instance.views.findAll {
-  //       view -> view.displayName != 'All'
-  //     }.each { view ->
-  //       if (view.contains(topLevelItem)) {
-  //         def viewName = view.displayName
-  //         echo "View: ${viewName} contains job: ${topLevelItem.displayName}"
-  //         return viewName
-  //       }
-  //     }
-  //   }
-  // }
-  return null
-}
-
-def getJobName() {
-  def currentJob = Jenkins.instance.getItemByFullName(currentBuild.fullProjectName)
-  if (currentJob.parent instanceof WorkflowMultiBranchProject) {
-    echo "multibranch"
-    echo "parentJobName=${currentJob.parent.displayName}"
-  } else {
-    echo "regular"
-    echo "jobName=${currentJob.displayName}"
-  }
-}
 
 pipeline {
   agent any
+  options {
+    disableConcurrentBuilds(abortPrevious: true)
+  }
   stages {
     stage('Hello') {
       steps {
         echo 'Hello World'
-        script {
-          getJobView()
-          getJobName()
-        }
       }
     }
   }
